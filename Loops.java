57f2f8a57f0bd19556bd16c590d5b597bfcb2c09
Pramodroc <pramodponduru3@gmail.com>
2026-03-08 02:50:36 +0530

    uncommented pattern code

---
+++ Loops.java
@@ -0,0 +1,55 @@
+//  public class Loops{
+//     public static void main(String[] args){
+// //         Scanner sc = new Scanner(System.in);
+// //         int n = sc.nextInt();
+
+
+// //         for(int i=0;i<n;i++){
+// //             if(i%2 == 0){
+// //                 System.out.println(i);
+// //             }
+// //         }
+// //     }
+// // }
+// for(; ;) {
+//             System.out.println("Apna College");
+//         }
+//     }
+// }
+
+
+
+import java.util.*;
+public class Loops{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int input = sc.nextInt();
+
+        if (input == 1){
+            System.out.println("user can enter marks : ");
+            int marks = sc.nextInt();
+        
+        if (marks >= 90){
+            System.out.println("This is good");
+
+        }
+        else if(89 >= marks && marks >= 60){
+            System.out.println("this is also good");
+
+        }
+        else if(59 >= marks && marks >= 0){
+            System.out.println("this is good as well");
+        }
+        else{
+            System.out.println("enter the valid marks");
+        }
+
+    }
+    if (input == 0){
+        System.out.println("you enter the wrong input");
+    }
+        
+
+
+    }
+}
---
+++ Patternquestions.java
@@ -0,0 +1,49 @@
+// public class Patternquestions {
+//     public static void main(String[] args) {
+        
+
+//         // int n = 7;
+//         // int m = 10;
+
+
+//         // for(int i=1;i<=n;i++){
+//         //     for(int j=1;j<=m;j++){
+//         //         System.out.print("*");
+//         //     }
+//         //     System.out.println();
+//         // }
+        
+//     }
+    
+// }
+
+
+
+
+public class Patternquestions{
+    public static void main(String[] args){
+
+
+        int n = 6;
+        int m = 10;
+
+
+
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i == 1 || j == 1 || i == n || j == m){
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+            
+        }
+    }
+
+}
